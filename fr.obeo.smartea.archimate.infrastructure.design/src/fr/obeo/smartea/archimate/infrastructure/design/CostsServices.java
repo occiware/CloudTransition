3fed26648ded4f717c6f0b981e105fec0f8220ec
William Piers <william.piers@obeo.fr>
2017-06-14 17:34:26 +0200

    fixed costs model location
    Change-Id: Ibd54818239ac8e5195e0ea484cc95c51b4895d4c

--- fr.obeo.smartea.archimate.infrastructure.design/src/fr/obeo/smartea/archimate/infrastructure/design/CostsServices.java
+++ fr.obeo.smartea.archimate.infrastructure.design/src/fr/obeo/smartea/archimate/infrastructure/design/CostsServices.java
@@ -10,12 +10,16 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
+import fr.obeo.smartea.core.basemm.BaseFactory;
+import fr.obeo.smartea.core.basemm.Folder;
 import fr.obeo.smartea.core.costs.Cost;
 import fr.obeo.smartea.core.costs.CostsContainer;
 import fr.obeo.smartea.core.costs.CostsFactory;
 import fr.obeo.smartea.core.costs.CostsPackage;
 
 public class CostsServices extends AbstractUIPlugin {
+
+	private static final String COSTS_FOLDER_NAME = "Costs";
 
 	public Collection<Cost> getCosts(EObject context) {
 		List<Cost> res = new ArrayList<Cost>();
@@ -36,12 +40,19 @@ public class CostsServices extends AbstractUIPlugin {
 	private CostsContainer getCostsContainer(EObject context) {
 		Resource resource = context.eResource();
 		for (EObject content : resource.getContents()) {
-			if (content instanceof CostsContainer) {
-				return (CostsContainer) content;
+			if (content instanceof Folder && ((Folder) content).getName().equals(COSTS_FOLDER_NAME)) {
+				for (EObject sub : ((Folder) content).getElements()) {
+					if (sub instanceof CostsContainer) {
+						return (CostsContainer) sub;
+					}
+				}
 			}
 		}
 		CostsContainer res = CostsFactory.eINSTANCE.createCostsContainer();
-		resource.getContents().add(res);
+		Folder costsFolder = BaseFactory.eINSTANCE.createFolder();
+		costsFolder.setName(COSTS_FOLDER_NAME);
+		costsFolder.getElements().add(res);
+		resource.getContents().add(costsFolder);
 		return res;
 	}
 }
