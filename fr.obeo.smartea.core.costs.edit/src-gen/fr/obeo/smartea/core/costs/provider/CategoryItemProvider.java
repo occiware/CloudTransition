b60288a4fc63e85311b95e2cdac8cd28bf0e1517
William Piers <william.piers@obeo.fr>
2017-09-11 10:38:11 +0200

    Updated costs.design
    Change-Id: I0da44726591c4d0a9135fc27a72cdcd0fdc9307b

--- fr.obeo.smartea.core.costs.edit/src-gen/fr/obeo/smartea/core/costs/provider/CategoryItemProvider.java
+++ fr.obeo.smartea.core.costs.edit/src-gen/fr/obeo/smartea/core/costs/provider/CategoryItemProvider.java
@@ -185,14 +185,14 @@ public class CategoryItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
 		String label = ((Category)object).getName();
 		return label == null || label.length() == 0 ?
 			getString("_UI_Category_type") : //$NON-NLS-1$
-			getString("_UI_Category_type") + " " + label; //$NON-NLS-1$ //$NON-NLS-2$
+			label; //$NON-NLS-1$ //$NON-NLS-2$
 	}
 	
 
