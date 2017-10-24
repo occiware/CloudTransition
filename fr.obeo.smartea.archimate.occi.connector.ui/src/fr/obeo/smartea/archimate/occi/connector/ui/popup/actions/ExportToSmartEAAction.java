0e3793e9cee014f7b4f362055c70760700d3e0d9
William Piers <william.piers@obeo.fr>
2017-10-24 15:15:55 +0200

    Fixed commit issue
    Change-Id: Ib068d11deedb05b21b9d6be5ddab610bd12822f1

--- fr.obeo.smartea.archimate.occi.connector.ui/src/fr/obeo/smartea/archimate/occi/connector/ui/popup/actions/ExportToSmartEAAction.java
+++ fr.obeo.smartea.archimate.occi.connector.ui/src/fr/obeo/smartea/archimate/occi/connector/ui/popup/actions/ExportToSmartEAAction.java
@@ -1,12 +1,7 @@
 package fr.obeo.smartea.archimate.occi.connector.ui.popup.actions;
 
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-
-import java.io.IOException;
-import java.util.Collections;
-
 import org.eclipse.cmf.occi.core.Configuration;
+import org.eclipse.emf.ecore.resource.Resource;
 
 import fr.obeo.smartea.archimate.occi.OCCI2Archi;
 import fr.obeo.smartea.archimate.occi.reconciler.ArchiReconciler;
@@ -23,17 +18,7 @@ public class ExportToSmartEAAction extends AbstractSmartEAAction {
 	protected void work(Configuration configuration, Resource semanticResource) {
 		Folder targetTechFolder = ModelUtils.getSubFolder(semanticResource, ModelUtils.TECHNOLOGY_FOLDER_NAME);
 		Folder converted = new OCCI2Archi().convert(configuration);
-		
-		Resource r = semanticResource.getResourceSet().createResource(URI.createURI("tmp"));
-		r.getContents().add(converted);
-		try {
-			r.save(System.out, Collections.EMPTY_MAP);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-//		new ArchiReconciler().reconcile(converted, targetTechFolder);
+		new ArchiReconciler().reconcile(converted, targetTechFolder);
 	}
 
 }
